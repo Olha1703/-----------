91067b9e77ec39aa851c973df2c4188a8a7262bd
Olha1703 <143745594+Olha1703@users.noreply.github.com>
2024-04-25 20:07:17 +0300

    Add files via upload

---
+++ HW 7 ES6.js
@@ -0,0 +1,27 @@
+// ----1----
+// Створіть функцію-конструктор Calculator, який створює об’єкти з трьома методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей.
+
+// function Calculator() {
+//   this.read = function () {
+//     this.a = +prompt("Введіть  число a");
+//     this.b = +prompt("Введіть  число b");
+//   };
+
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+
+//   this.mul = function () {
+//     return this.a * this.b;
+//   };
+// }
+
+// const calc = new Calculator();
+
+// calc.read();
+
+// alert("Sum дорівнює " + calc.sum());
+// alert("Mul дорівнює " + calc.mul());
